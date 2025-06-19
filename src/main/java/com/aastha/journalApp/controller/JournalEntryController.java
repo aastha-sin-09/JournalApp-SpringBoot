c5884e935ad0d46e13603e8ad7155d0e947dd2db
aastha-sin-09 <136198084+aastha-sin-09@users.noreply.github.com>
2025-06-20 00:13:21 +0530

    add basic CRUD REST API

---
+++ src/main/java/com/aastha/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,39 @@
+package com.aastha.journalApp.controller;
+
+import com.aastha.journalApp.entity.JournalEntry;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll(){
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(), myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("/id/{id}")
+    public JournalEntry updateJournalEntryById(@PathVariable Long id, @RequestBody JournalEntry myEntry){
+        return journalEntries.put(id, myEntry);
+    }
+}
