a96fac2f0c3fa4b9f5b6efad9e3f65830d43e2db
aastha-sin-09 <136198084+aastha-sin-09@users.noreply.github.com>
2025-08-07 00:18:12 +0530

    Fix: Update CORS configuration to allow requests from new frontend URL

--- src/main/java/com/aastha/journalApp/config/GlobalCorsConfig.java
+++ src/main/java/com/aastha/journalApp/config/GlobalCorsConfig.java
@@ -7,17 +7,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class GlobalCorsConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "https://journal-ss-frontend.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // 👈 This is important if you're using cookies/JWT
+                        .allowCredentials(true);
             }
         };
     }
